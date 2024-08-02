59fbe614da71f6f9bc5ab1c5ab9cdebc448b3703
daradanilova <lemma1102@gmail.com>
2024-08-02 14:07:25 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const input = document.querySelectorAll("input");
+input.forEach((element)=>{
+    const label = document.createElement("label");
+    const labelName = element.getAttribute("name");
+    const labelText = (labelName.charAt(0).toUpperCase() + labelName.slice(1)).split(/(?=[A-Z])/).join(" ");
+    const placeholder = labelText;
+    label.textContent = labelText;
+    label.classList.add("field-label");
+    label.setAttribute("for", element.id);
+    element.parentElement.insertBefore(label, element);
+    element.setAttribute("placeholder", placeholder);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
